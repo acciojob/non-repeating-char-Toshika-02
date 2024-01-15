e88d0391e091c1b4c8f68b5b2300befdd2df784f
Toshika-02 <105552050+Toshika-02@users.noreply.github.com>
2024-01-15 17:45:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	const freq = {}
+	for(const ch of inputStr){
+		freq[ch] = (freq[ch] || 0)+1
+	}
+	 for(const ch of inputStr){
+		 if(freq[ch]===1){
+			 return ch
+		 }
+	 }
+	  return null;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
